82676a8229549dfeb58a276b95d8680c8595d0d6
김준영 <novb1492@naver.com>
2022-09-05 13:25:37 +0900

    리덕스는 액션을 좀 특이하게 사용한다 리덕스 툴킷을 공부해봐야할듯

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
+import { legacy_createStore as createStore } from "redux";
 let plus=document.querySelector('#plus');
 let num=document.querySelector('#num');
 let minus=document.querySelector('#minus');
+let reducer=(state = 0,action)=>{
+  if(action.type==='add'){
+    state+=1;
+  }else if(action.type==='minus'){
+    state-=1;
+  }
+  return state;
+};
+let store=createStore(reducer);
 
-let count=0;
-
-num.innerHTML=count;
-
-function updateText() {
-  num.innerHTML=count;
-}
-function add() {
-  count=count+1;
-  updateText();
-}
-function minusF() {
-  count=count-1;
-  updateText();
-}
-
-plus.addEventListener("click",add);
-minus.addEventListener("click",minusF);
-
+store.dispatch({type:'add'});
+console.log(store.getState());
+store.dispatch({type:'minus'});
+console.log(store.getState());
